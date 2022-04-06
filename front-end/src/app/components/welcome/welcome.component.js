0f82311fd5000edb52d080b6f537de33ee29a73b
BaptisteZloch <bzloch@hotmail.fr>
2022-04-06 11:53:53 +0200

    updated files js to es6

--- front-end/src/app/components/welcome/welcome.component.js
+++ front-end/src/app/components/welcome/welcome.component.js
@@ -1,44 +1,49 @@
 // TODO Step 7 import { Component } from "../../utils/component";
 // TODO Step 7 import template from  "./welcome.component.html"
 
-(function() {   // TODO Step 7 remove this closure
+(function () {
+  // TODO Step 7 remove this closure
 
-    // TODO Step 3.1 create a class
-    /* class WelcomeComponent constructor  */
-    function WelcomeComponent() {
-    }
-
+  // TODO Step 3.1 create a class
+  /* class WelcomeComponent constructor  */
+  class WelcomeComponent {
     /* method WelcomeComponent.init */
-    WelcomeComponent.prototype.init = function init() {
-        var form = document.querySelector('form.form-signin');
+    init() {
+      var form = document.querySelector("form.form-signin");
 
-        form.addEventListener('submit', function(event) {     // TODO Step 3.2: use arrow function
+      form.addEventListener(
+        "submit",
+        function (event) {
+          // TODO Step 3.2: use arrow function
 
-            event.preventDefault();
-            if (form.checkValidity() === false) {
-                event.stopPropagation();
-                form.classList.add('was-validated');
-            } else {
-                var name = event.srcElement.querySelector('#nickname').value;
-                var size = parseInt(event.srcElement.querySelector('#size').value);
+          event.preventDefault();
+          if (form.checkValidity() === false) {
+            event.stopPropagation();
+            form.classList.add("was-validated");
+          } else {
+            var name = event.srcElement.querySelector("#nickname").value;
+            var size = parseInt(event.srcElement.querySelector("#size").value);
 
-                _startGame(name, size);
-            }
-        }.bind(this), false);
-
-        return this;
+            _startGame(name, size);
+          }
+        }.bind(this),
+        false
+      );
+      return this;
     }
+  }
 
-    // TODO Step 7 implement getTemplate() {}
+  // TODO Step 7 implement getTemplate() {}
 
-    function _startGame(name, size) {
-        // TODO Step 1: replace with '../game/game.component.html?name=...'
-        // TODO Step 3.2: use template literals (backquotes)
-        // TODO Step 7: change path to: `game?name=${name}=name&size=${size}`
-        window.location = '../game/game.component.html?name=' + name + '&size=' + size;
-    }
+  function _startGame(name, size) {
+    // TODO Step 1: replace with '../game/game.component.html?name=...'
+    // TODO Step 3.2: use template literals (backquotes)
+    // TODO Step 7: change path to: `game?name=${name}=name&size=${size}`
+    window.location =
+      "../game/game.component.html?name=" + name + "&size=" + size;
+  }
 
-    // put component in global scope, to be runnable right from the HTML.
-    // TODO Step 7 export WelcomeComponent
-    window.WelcomeComponent = WelcomeComponent
+  // put component in global scope, to be runnable right from the HTML.
+  // TODO Step 7 export WelcomeComponent
+  window.WelcomeComponent = WelcomeComponent;
 })();
