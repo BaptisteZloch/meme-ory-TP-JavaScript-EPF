a814655eafac808af9b517dc91d0f5b80156c688
BaptisteZloch <bzloch@hotmail.fr>
2022-04-25 11:25:52 +0200

    removed all comments

--- front-end/src/app/components/game/game.component.js
+++ front-end/src/app/components/game/game.component.js
@@ -6,159 +6,143 @@ import "./game.component.scss";
 import * as localforage from "localforage/dist/localforage";
 
 const environment = {
-  api: {
-    host: "http://localhost:8081",
-  },
+    api: {
+        host: "http://localhost:8081",
+    },
 };
 export class GameComponent extends Component {
-  constructor() {
-    super("game");
-    let params = parseUrl();
+    constructor() {
+        super("game");
+        const params = parseUrl();
 
-    // save player name & game ize
-    this._name = params.name;
-    this._size = parseInt(params.size) || 9;
-    this._flippedCard = null;
-    this._matchedPairs = 0;
-  }
-
-  getTemplate() {
-    return template;
-  }
-
-  gotoScore() {
-    const timeElapsedInSeconds = Math.floor(
-      (Date.now() - this._startTime) / 1000
-    );
-
-    setTimeout(() => {
-      localforage.clear();
-      window.location.hash = `score?name=${this._name}&size=${this._size}'&time=${timeElapsedInSeconds}`;
-    }, 750);
-  }
-  async init() {
-    // fetch the cards configuration from the server
-    const config = await this.fetchConfig();
-    //.then((config) => {
-    // TODO Step 3.2: use arrow function
-    this._config = config;
-
-    // create a card out of the config
-    this._cards = []; // TODO Step 3.3: use Array.map()
-
-    this._config.ids.map((id) => this._cards.push(new CardComponent(id)));
-
-    this._boardElement = document.querySelector(".cards");
-
-    this._cards.forEach((element) => {
-      // TODO Step 3.2: use arrow function
-      const card = element;
-      this._boardElement.appendChild(element.getElement());
-      card.getElement().addEventListener("click", () => {
-        this._flipCard(card);
-      }); // TODO Step 3.2 use arrow function.
-    });
-    this.start();
-    //});
-  }
-
-  // TODO Step 7 implement getTemplate() {}
-
-  start() {
-    this._startTime = Date.now();
-    let seconds = 0;
-    // TODO Step 3.2: use template literals (backquotes)
-    document.querySelector("nav .navbar-title").textContent = `Player: ${
-      this._name
-    }. Elapsed time: ${seconds++}`;
-
-    this._timer = setInterval(() => {
-      // TODO Step 3.2: use arrow function
-      // TODO Step 3.2: use template literals (backquotes)
-      document.querySelector("nav .navbar-title").textContent = `Player: ${
-        this._name
-      }. Elapsed time: ${seconds++}`;
-    }, 1000);
-  }
-
-  async fetchConfig() {
-    return await fetch(`${environment.api.host}/board?size=${this._size}`, {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .catch((error) => console.log("Error while fetching   config: ", error));
-  }
-
-  _flipCard(card) {
-    if (this._busy) {
-      return;
-    }
-
-    if (card.flipped) {
-      return;
-    }
-
-    localforage
-      .setItem(
-        "cards",
-        this._cards.map((card) => card["_id"])
-      )
-      .then(function () {
-        return localforage.getItem("cards");
-      })
-      .then(function (value) {
-        console.log(value);
-      })
-      .catch(function (err) {});
-
-    localforage
-      .setItem("time", Math.floor((Date.now() - this._startTime) / 1000))
-      .then(function () {
-        return localforage.getItem("time");
-      })
-      .then(function (value) {
-        console.log(value);
-      })
-      .catch(function (err) {});
-
-    // flip the card
-    card.flip();
-
-    // if flipped first card of the pair
-    if (!this._flippedCard) {
-      // keep this card flipped, and wait for the second card of the pair
-      this._flippedCard = card;
-    } else {
-      // second card of the pair flipped...
-
-      // if cards are the same
-      if (card.equals(this._flippedCard)) {
-        this._flippedCard.matched = true;
-        card.matched = true;
-        this._matchedPairs += 1;
-
-        // reset flipped card for the next turn.
+        // save player name & game ize
+        this._name = params.name;
+        this._size = parseInt(params.size) || 9;
         this._flippedCard = null;
-
-        if (this._matchedPairs === this._size) {
-          this.gotoScore();
-        }
-      } else {
-        this._busy = true;
-
-        // cards did not match
-        // wait a short amount of time before hiding both cards
-        // TODO Step 3.2 use arrow function
-        setTimeout(() => {
-          // hide the cards
-          this._flippedCard.flip();
-          card.flip();
-          this._busy = false;
-
-          // reset flipped card for the next turn.
-          this._flippedCard = null;
-        }, 500);
-      }
+        this._matchedPairs = 0;
     }
-  }
+
+    getTemplate() {
+        return template;
+    }
+
+    gotoScore() {
+        const timeElapsedInSeconds = Math.floor(
+            (Date.now() - this._startTime) / 1000
+        );
+
+        setTimeout(() => {
+            localforage.clear();
+            window.location.hash = `score?name=${this._name}&size=${this._size}'&time=${timeElapsedInSeconds}`;
+        }, 750);
+    }
+    async init() {
+        // fetch the cards configuration from the server
+        const config = await this.fetchConfig();
+        this._config = config;
+
+        // create a card out of the config
+        this._cards = [];
+        this._config.ids.map((id) => this._cards.push(new CardComponent(id)));
+
+        this._boardElement = document.querySelector(".cards");
+
+        this._cards.forEach((element) => {
+            const card = element;
+            this._boardElement.appendChild(element.getElement());
+            card.getElement().addEventListener("click", () => {
+                this._flipCard(card);
+            });
+        });
+        this.start();
+        //});
+    }
+
+    start() {
+        this._startTime = Date.now();
+        let seconds = 0;
+        document.querySelector("nav .navbar-title").textContent = `Player: ${this._name}. Elapsed time: ${seconds++}`;
+
+        this._timer = setInterval(() => {
+            document.querySelector("nav .navbar-title").textContent = `Player: ${this._name}. Elapsed time: ${seconds++}`;
+        }, 1000);
+    }
+
+    async fetchConfig() {
+        return await fetch(`${environment.api.host}/board?size=${this._size}`, {
+                method: "GET",
+            })
+            .then((response) => response.json())
+            .catch((error) => console.log("Error while fetching   config: ", error));
+    }
+
+    _flipCard(card) {
+        if (this._busy) {
+            return;
+        }
+
+        if (card.flipped) {
+            return;
+        }
+
+        localforage
+            .setItem(
+                "cards",
+                this._cards.map((card) => card["_id"])
+            )
+            .then(() => {
+                return localforage.getItem("cards");
+            })
+            .then((value) => {
+                console.log(value);
+            })
+            .catch((err) => {});
+
+        localforage
+            .setItem("time", Math.floor((Date.now() - this._startTime) / 1000))
+            .then(() => localforage.getItem("time"))
+            .then((value) => {
+                console.log(value);
+            })
+            .catch((err) => {});
+
+        // flip the card
+        card.flip();
+
+        // if flipped first card of the pair
+        if (!this._flippedCard) {
+            // keep this card flipped, and wait for the second card of the pair
+            this._flippedCard = card;
+        } else {
+            // second card of the pair flipped...
+
+            // if cards are the same
+            if (card.equals(this._flippedCard)) {
+                this._flippedCard.matched = true;
+                card.matched = true;
+                this._matchedPairs += 1;
+
+                // reset flipped card for the next turn.
+                this._flippedCard = null;
+
+                if (this._matchedPairs === this._size) {
+                    this.gotoScore();
+                }
+            } else {
+                this._busy = true;
+
+                // cards did not match
+                // wait a short amount of time before hiding both cards
+                setTimeout(() => {
+                    // hide the cards
+                    this._flippedCard.flip();
+                    card.flip();
+                    this._busy = false;
+
+                    // reset flipped card for the next turn.
+                    this._flippedCard = null;
+                }, 500);
+            }
+        }
+    }
 }
--- front-end/src/app/components/welcome/welcome.component.js
+++ front-end/src/app/components/welcome/welcome.component.js
@@ -3,33 +3,33 @@ import template from "./welcome.component.html";
 import './welcome.component.scss'
 export class WelcomeComponent extends Component {
 
-  constructor(){
-    super("welcome");
-  }
-  init() {
-    const form = document.querySelector("form.form-signin");
-    form.addEventListener(
-      "submit",
-      (event) => {
-        event.preventDefault();
-        if (form.checkValidity() === false) {
-          event.stopPropagation();
-          form.classList.add("was-validated");
-        } else {
-          var name = event.srcElement.querySelector("#nickname").value;
-          var size = parseInt(event.srcElement.querySelector("#size").value);
+    constructor() {
+        super("welcome");
+    }
+    init() {
+        const form = document.querySelector("form.form-signin");
+        form.addEventListener(
+            "submit",
+            (event) => {
+                event.preventDefault();
+                if (form.checkValidity() === false) {
+                    event.stopPropagation();
+                    form.classList.add("was-validated");
+                } else {
+                    var name = event.srcElement.querySelector("#nickname").value;
+                    var size = parseInt(event.srcElement.querySelector("#size").value);
 
-          _startGame(name, size);
-        }
-      },
-      false
-    );
-    return this;
-  }
+                    _startGame(name, size);
+                }
+            },
+            false
+        );
+        return this;
+    }
 
-  getTemplate() { return template; }
+    getTemplate() { return template; }
 }
 
-function _startGame(name, size) {
-  window.location.hash = `game?name=${name}=name&size=${size}`;
+const _startGame = (name, size) => {
+    window.location.hash = `game?name=${name}=name&size=${size}`;
 }
